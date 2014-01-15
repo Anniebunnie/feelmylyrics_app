7f82c12f7dda0f9892348fa88037f1306a8aa7c2
Anniebunnie <aj.kim84@gmail.com>
2014-01-15 14:24:31 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeelmylyricsApp::Application.config.secret_token = '524be9a775057ef1af23d65913a8bfe2ec101d909e5fe1f144c3f1e2032e3dae4b9af19e9e17b4d0b096ce454c3d0cec15c1925e72be989f7828cd5554998c41'
